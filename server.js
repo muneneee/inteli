56ceafcf8d18156e93f97c2c795d80eb6fee331b
[muneneee] <[lukekevin39@gmail.com]>
2020-08-12 09:17:33 +0300

    deployment

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.use(express.static('./dist/summary'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/summary/'}),
-);
+app.get('/*', (req, res) =>{
+    res.sendFile(`./front-end/dist/index.html`);
+});
+
 
 app.listen(process.env.PORT || 8080);
