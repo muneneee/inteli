661f3a66c4395893ef684ac49ef121e5c3776b67
[muneneee] <[lukekevin39@gmail.com]>
2020-08-12 12:40:08 +0300

    deploy

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/summary'));
+app.use(express.static(__dirname + '/dist/Summary'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/summary/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/Summary/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
